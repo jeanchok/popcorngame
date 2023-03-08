7f69e82d731fee19b69f562c1d480618fd1016ad
jeanchok <48122647+jeanchok@users.noreply.github.com>
2023-03-08 14:48:29 +0100

    Update socket.js

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -24,7 +24,7 @@ export function SocketContextProvider({ children }) {
         }));
         isSecondRender.current = true;
         console.log(process.env.REACT_APP_API_URL, socket)
-    }, [process.env.REACT_APP_API_URL]);
+    }, []);
 
 
 
